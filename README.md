c625e9f799d7cd7bfe7d29b858ac9090f61546c6
Argonaut <gaming.genos1729@gmail.com>
2023-07-15 16:57:09 +0530

    Setup file is created and readme is updated.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SpotifyUtil
+SpotifyUtils is a very useful library made over Spotipy to automate some rather tiring tasks.
+
+## Features
+- Add songs to a playlist/album (creates one if name is provided instead of playlist url)
+- Add songs to a playlist/album from a text file if it has the tracks' name and artist ("Name - Artist" format) in a newline separated way.
+- Removes duplicates while adding songs if the flag is set to true.
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SpotifyUtil",
+    version="1.0.0",
+    author="Arg0naut18",
+    description="SpotifyUtils is a very useful library made over Spotipy to automate some rather tiring tasks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Arg0naut18/SpotifyUtil",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha"
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">= 3.6",
+    include_package_data=True,
+    install_requires=["spotipy", "tkinter"]
+)
