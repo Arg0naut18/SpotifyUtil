509fb619eaf2748b71e0e679b8c525102b44b228
Argonaut <gaming.genos1729@gmail.com>
2023-07-23 15:37:55 +0530

    Adding both test and public version of wheels

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
 
 setuptools.setup(
     name="SpotifyUtil",
-    version="0.0.4",
+    version="0.1.1",
     author="Arg0naut18",
     description="SpotifyUtils is a very useful library made over Spotipy to automate some rather tiring tasks.",
     long_description=long_description,
